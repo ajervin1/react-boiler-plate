574ce4e700f14f9bfbf43f870b40256d444a1374
ajervin1 <anthony.ervin100@gmail.com>
2023-07-28 18:36:50 -0700

    Added React Router, TailWind

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
+import { Route, Router, Routes } from 'react-router-dom'
 
 import './App.css';
-
+import HomePage from "./pages/HomePage";
+import DetailsPage from "./pages/DetailsPage";
 function App() {
-	return (
-		<div className="App">
-
-		</div>
+	return (<div className="App">
+			<Routes>
+				<Route path={'/'} element={ <HomePage  />} />
+				<Route path={'/details'} element={<DetailsPage />} />
+			</Routes>
+	</div>
 	);
 }
 
